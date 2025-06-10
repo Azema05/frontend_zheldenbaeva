2e2aa0fbfd60bb9f6ed7568d8b4f7c17d15896e2
He_vajno <111884984+Azema05@users.noreply.github.com>
2025-06-10 20:24:25 +0000

    task4

---
+++ ts/scripts.ts
@@ -0,0 +1,39 @@
+function showCard(card) {
+  let infCards = document.getElementById("information-cards");
+
+  if (!infCards) {
+    console.error("Элемент #information-cards не найден!");
+    return;
+  }
+
+  const newCard = `
+    <div class="card">
+      <p class="card_text">${card.body}</p>
+      <div class="card_footer">
+        <img class="card_avatar" src="/img/image 9.png" alt="avatar">
+        <p class="card_person">
+          <span class="card_person-name">${card.name}</span>
+          <small class="card_person-type">User</small>
+        </p>
+      </div>
+    </div>
+  `;
+  infCards.innerHTML += newCard;
+}
+
+function showAllCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    showCard(cards[i]);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://jsonplaceholder.typicode.com/comments?_limit=4')
+    .then(response => response.json())
+    .then(data => {
+      console.log('Полученные данные:', data);
+      showAllCards(data);
+    })
+    .catch(error => console.error('Ошибка при получении данных:', error));
+});
+
